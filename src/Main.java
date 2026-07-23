ab983a9be2c952e79d13ad73f3b831a6f5ee35f6
heisttt <nicoleevlgg@gmail.com>
2026-07-22 22:54:38 -0500

    feat: agregar ejecución principal del sistema

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public static void main(String[] args) {
+
+        Pedido pedido = new Pedido();
+
+        // Primera bebida: Latte con chocolate y shot extra.
+        Bebida bebida1 = new Latte();
+        bebida1 = new Chocolate(bebida1);
+        bebida1 = new ShotExtra(bebida1);
+
+        // Segunda bebida: Espresso con leche.
+        Bebida bebida2 = new Espresso();
+        bebida2 = new Leche(bebida2);
+
+        // Tercera bebida: Americano con canela y shot extra.
+        Bebida bebida3 = new Americano();
+        bebida3 = new Canela(bebida3);
+        bebida3 = new ShotExtra(bebida3);
+
+        pedido.agregarBebida(bebida1);
+        pedido.agregarBebida(bebida2);
+        pedido.agregarBebida(bebida3);
+
+        pedido.mostrarDetalle();
     }
 }
