46efbfb0d042899d5590c971a6e2dbedab87f5ec
heisttt <nicoleevlgg@gmail.com>
2026-07-22 22:50:54 -0500

    feat: agregar clase latte

---
+++ src/Latte.java
@@ -0,0 +1,12 @@
+public class Latte implements Bebida {
+
+    @Override
+    public String getDescripcion() {
+        return "Latte";
+    }
+
+    @Override
+    public double getPrecio() {
+        return 8.00;
+    }
+}
